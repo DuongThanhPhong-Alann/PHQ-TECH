620be0cd1fce9496264356886acd94a1f0bef508
Duong Thanh Phong <duongthanhphong1618@gmail.com>
2025-05-13 15:19:31 +0700

    thời tiết + vá lỗi

---
+++ APT/APT/ViewComponents/WeatherViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace QLCCCC.ViewComponents
+{
+    public class WeatherViewComponent : ViewComponent
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<IViewComponentResult> InvokeAsync(string city = "Ho Chi Minh")
+        {
+            string apiKey = "96e280e6c6f5ff134d49420a461773b2";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric&lang=vi";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+                return View("Error");
+
+            var json = await response.Content.ReadAsStringAsync();
+            JObject data = JObject.Parse(json);
+
+            ViewBag.City = data["name"];
+            ViewBag.Temp = data["main"]["temp"];
+            ViewBag.FeelsLike = data["main"]["feels_like"];
+            ViewBag.Humidity = data["main"]["humidity"];
+            ViewBag.Description = data["weather"][0]["description"];
+            ViewBag.Icon = data["weather"][0]["icon"];
+
+            return View("Default");
+        }
+    }
+}
